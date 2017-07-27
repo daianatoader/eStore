e9c9e619cd56c085a2b178233213ac8b90f41533
Carina <carina_matei@yahoo.com>
2017-07-27 12:36:19 +0300

    change

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -1,6 +1,32 @@
 package main;
 
+import com.fasterxml.classmate.AnnotationConfiguration;
+import model.dataaccess.BrandDAO;
+import model.dataaccess.IBrandDAO;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import model.entities.*;
 
 public class MainClass {
+    public static SessionFactory factory;
+    public static void main(String[] args){
 
+        try{
+            factory = new Configuration().
+                    configure().
+                    addPackage("model.entities"). //add package if used.
+                    addAnnotatedClass(Brand.class).
+                    buildSessionFactory();
+        }catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+        IBrandDAO bdao = new BrandDAO();
+        Brand brand = new Brand("Samsung","electronice");
+        bdao.add(brand);
+    }
 }
